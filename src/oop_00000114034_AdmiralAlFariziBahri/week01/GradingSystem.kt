400617016603b65900b5aacf79a2fb2b55413a26
izijerry <izijerry56@gmail.com>
2026-02-04 00:10:26 +0700

    weel01: initial setup and procedural code

---
+++ src/oop_00000114034_AdmiralAlFariziBahri/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000114034_AdmiralAlFariziBahri.week01
+
+fun main(){
+    var name: String = "jhon thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
