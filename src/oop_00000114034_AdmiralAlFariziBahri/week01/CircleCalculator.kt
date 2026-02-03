5b102ec50a22b203ea6a391f678b03b444efc06d
izijerry <izijerry56@gmail.com>
2026-02-04 01:02:22 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000114034_AdmiralAlFariziBahri/week01/CircleCalculator.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri/week01/CircleCalculator.kt
@@ -1,14 +1,10 @@
 package oop_00000114034_AdmiralAlFariziBahri.week01
 
 fun main() {
-    // Variable definition
     val radius = 7.0
     val pi = 3.14
-    // Calculation
     var area: Double = pi * radius * radius
-    // Output Concatenation
     println("Radius: $radius, Area: $area")
-    // Logic check
     println("check size: ${checkSize(area)}")
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
