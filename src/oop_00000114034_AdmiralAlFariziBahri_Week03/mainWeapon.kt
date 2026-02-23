d0d366d1d1150d338231b8e01fe15c4dd8af8287
izijerry <izijerry56@gmail.com>
2026-02-23 12:08:49 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000114034_AdmiralAlFariziBahri_Week03/mainWeapon.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week03/mainWeapon.kt
@@ -3,7 +3,8 @@ package oop_00000114034_AdmiralAlFariziBahri_Week03
 fun main(){
     val t = Weapon("tes")
 
-    t.damage = 600
+    t.damage = -50
+    t.damage = 9999
 
     println("damage: ${t.damage}, tier: ${t.tier}")
 }
