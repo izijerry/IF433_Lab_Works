85afe3f65650429ce7d8c43b00568cb687685ef4
izijerry <izijerry56@gmail.com>
2026-02-24 15:16:11 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week04/Car.kt
@@ -0,0 +1,7 @@
+package oop_00000114034_AdmiralAlFariziBahri_Week04
+
+class Car (brand:String, val numberOfDoors: Int) :Vehicle(brand) {
+    fun openTrunk(){
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibukua.")
+    }
+}
