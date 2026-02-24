ad2722579d166cb8d15277a9b5f53f622f2e4bcc
izijerry <izijerry56@gmail.com>
2026-02-24 15:18:21 +0700

    week04: implement super keyword in Car accelerate

--- src/oop_00000114034_AdmiralAlFariziBahri_Week04/Car.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week04/Car.kt
@@ -8,4 +8,10 @@ class Car (brand:String, val numberOfDoors: Int) :Vehicle(brand) {
     override fun honk(){
         println("tin tin tin mobil $brand lewat")
     }
+
+    override fun accelerate(){
+        super.accelerate()
+        println("Mobil $brand menggunakan transmisi gigi untuk menambah kecepatan.")
+
+    }
 }
