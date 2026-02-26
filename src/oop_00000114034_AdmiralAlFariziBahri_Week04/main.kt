a14d96f3f3025a6f7be7f4b8204e4c8f61a112b9
izijerry <izijerry56@gmail.com>
2026-02-26 07:25:36 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000114034_AdmiralAlFariziBahri_Week04/main.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week04/main.kt
@@ -12,7 +12,18 @@ fun main(){
     mycar.honk()
     mycar.accelerate()
 
+    println("\n --- testing electric car ---")
     val ecar = ElectricCar("wuling", 4, 90)
     ecar.accelerate()
+
+    println("\n --- testing manager ---")
+    val mngr = Manager("joni", 2500000)
+    mngr.work()
+    mngr.calculateBonus()
+
+    println("\n --- testing developer ---")
+    val dev = Developer("jim", 5000000, "C")
+    dev.work()
+    dev.calculateBonus()
 
 }
