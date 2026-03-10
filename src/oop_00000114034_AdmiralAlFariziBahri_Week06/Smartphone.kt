ad5c5db7a1e760e468d92edb8dd482f0058ae7ae
izijerry <izijerry56@gmail.com>
2026-03-10 15:06:16 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000114034_AdmiralAlFariziBahri_Week06/Smartphone.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week06/Smartphone.kt
@@ -1,5 +1,9 @@
 package oop_00000114034_AdmiralAlFariziBahri_Week06
 
 class Smartphone: Camera, Phone {
-
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("sistem operasi smartphone berhasil booting")
+    }
 }
