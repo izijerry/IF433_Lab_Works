904d936f54334d86cb974f4577da431da04f07ac
izijerry <izijerry56@gmail.com>
2026-03-10 15:50:58 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week06/MainTugas.kt
@@ -0,0 +1,7 @@
+package oop_00000114034_AdmiralAlFariziBahri_Week06
+
+fun MainTugas() {
+    val lamp = SmartLamp("123", "Ruang Tamu")
+    val speaker = SmartSpeaker("123", "Google Nest Dapur")
+    val cctv = SmartCCTV("123", "Ezviz Garasi")
+}
