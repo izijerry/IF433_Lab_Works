8f013d52f31b615ebba2a5650a6b17f74dac6b66
izijerry <izijerry56@gmail.com>
2026-03-10 15:47:33 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000114034_AdmiralAlFariziBahri_Week06/SmartHomeHub.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week06/SmartHomeHub.kt
@@ -9,8 +9,18 @@ class SmartHomeHub {
 
     fun turnOffAllSwitches() {
         for (device in devices) {
-            if(devices is Switchable){
-                devices.turnOff()
+            if(device is Switchable){
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode(){
+        for (device in devices) {
+            if (device is Recordable){
+                device.startRecord()
+            }else if(device is SmartSpeaker){
+                device.playMusic("sirine peringatan")
             }
         }
     }
