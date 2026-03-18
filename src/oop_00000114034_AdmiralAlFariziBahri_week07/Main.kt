8d867b2ba064b9580663ba46437666de135a8fd4
izijerry <izijerry56@gmail.com>
2026-03-18 14:24:25 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000114034_AdmiralAlFariziBahri_week07/Main.kt
+++ src/oop_00000114034_AdmiralAlFariziBahri_week07/Main.kt
@@ -1,6 +1,6 @@
 package oop_00000114034_AdmiralAlFariziBahri_week07
 
-fun Main() {
+fun main() {
     println("=== TEST SINGLETON ===")
     println("Status: ${DatabaseManager.connectionStatus}")
     DatabaseManager.connect()
@@ -9,8 +9,15 @@ fun Main() {
     val client = NetworkClient.createClient()
 
     println("\n=== TEST REGULAR CLASS ===")
-    val data1 = RegularUser("Alice", 28)
-    val data2 = RegularUser("Alice", 28)
+    val data1 = Datauser("Alice", 28)
+    val data2 = Datauser("Alice", 28)
     println(data1)
     println("sama? ${data1 == data2}")
+
+
+    val data3 = data1.copy(age = 28)
+    println("hasil copy data3: $data3")
+
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur userAge: $userAge")
 }
