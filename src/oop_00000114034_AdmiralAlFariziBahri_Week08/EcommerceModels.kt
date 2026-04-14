185a5667bbcacb2ca66ef32529fd233cb3a0434c
wira4512 <wbekti30@gmail.com>
2026-04-14 16:00:39 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week08/EcommerceModels.kt
@@ -0,0 +1,14 @@
+package oop_00000114034_AdmiralAlFariziBahri_week08
+
+sealed class Product
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
