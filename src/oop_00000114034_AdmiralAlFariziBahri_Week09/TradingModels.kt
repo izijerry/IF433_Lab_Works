449dbf28fe9aaefcd531fa3fe118232e258a01fe
izi <izijerry@gmail.com>
2026-05-12 16:34:11 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000114034_AdmiralAlFariziBahri_Week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000114034_AdmiralAlFariziBahri_Week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
