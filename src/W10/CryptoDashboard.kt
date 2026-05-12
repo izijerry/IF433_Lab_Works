ec17de9c96fb184f177705c74a4d6587cba9de97
izi <izijerry@gmail.com>
2026-05-12 17:26:21 +0700

    week10: (task) initialize and populate transaction repository

--- src/W10/CryptoDashboard.kt
+++ src/W10/CryptoDashboard.kt
@@ -2,16 +2,16 @@ package W10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
-    val txRepo = WalletRepository<Transaction>()
 
+    val txRepo = WalletRepository<Transaction>()
 
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 1000.0))
 
+
     txRepo.add(Transaction("TX001", 100.0))
     txRepo.add(Transaction("TX002", 250.0))
-
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
 
