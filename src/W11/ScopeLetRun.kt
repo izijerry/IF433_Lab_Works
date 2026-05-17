e26d68d5ea0fd62b193866226294c25f95bcdcd2
izi <izijerry@gmail.com>
2026-05-17 15:54:53 +0700

    week11: implement let function for null safety

---
+++ src/W11/ScopeLetRun.kt
@@ -0,0 +1,13 @@
+package W11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+
+    println("Panjang nama: $length")
+}
