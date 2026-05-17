39c42173f10d3199cd7a58f5036cdd8456998e90
izi <izijerry@gmail.com>
2026-05-17 16:22:38 +0700

    week12: (task) handle feeding failure using onFailure

--- src/W12/SmartFeeder.kt
+++ src/W12/SmartFeeder.kt
@@ -69,6 +69,11 @@ fun main() {
             "Makan sore sukses! Sisa stok kibble: " +
                     "$currentKibbleStock gr"
         )
+    }.onFailure { error ->
+        println(
+            "Peringatan ke Pemilik: ${error.message}"
+        )
+        println("(Opsional) Berikan chicken jerky secara manual")
     }
 
 }
